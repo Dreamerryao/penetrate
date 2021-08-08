d4207be14a10bb4a81f254df95beae9f3247b078
dreamerryao <zuobinwang3@gmail.com>
2021-08-08 14:11:06 +0800

    upd: 更改亿点点 回去睡觉了

--- src/constants/entry.ts
+++ src/constants/entry.ts
@@ -1,12 +1,17 @@
 import type { CardLayoutProps } from "../layouts/CardLayout";
 import test from "../assets/test.jpg";
+
+enum ID_LIST {
+  COLOR_TEST = "color_test",
+}
+
 const entryArray: CardLayoutProps[] = [
   {
+    id: "color_test",
     imgSrc: test,
     title: "颜色性格测试",
     desc: '相关材料来自"乐嘉性格色彩快速测试"',
-    id: "color_test",
   },
 ];
 
-export { entryArray };
+export { entryArray, ID_LIST };
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
-
 import App from "./App";
-
 //import './App.css';
 import "./index.scss";
 
